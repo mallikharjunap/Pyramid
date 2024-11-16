1aff587106dfe274a31118517a6b02ef33ac1cb8
mallikharjunap <39264067+mallikharjunap@users.noreply.github.com>
2024-11-16 14:12:33 +0530

    code modification Left and Right Pyramids

--- RightPyramid/RightPyramid/ViewController.swift
+++ RightPyramid/RightPyramid/ViewController.swift
@@ -12,17 +12,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        rightPyramid(number: 10)
+        leftPyramid(number: 10)
+        
+    }
+    
+
+    func rightPyramid(number:Int) {
+        // Right Pyramid
         for i in 1..<10 {
-            for j in 1...i {
+            for _ in 1...i {
                // value = value + 1
                 print("*",terminator: " ")
             }
             print("")
             
         }
+        print("===========================================")
     }
     
-
+    func leftPyramid(number:Int) {
+        // left pyramid
+        for i in 1..<10 {
+            
+            for _ in stride(from: 10, to: i, by: -1)
+            {
+                print(terminator: "  ")
+            }
+            for _ in 1...i{
+                print("*", terminator:" ")
+            }
+            print("")
+        }
+        
+        print("===========================================")
+    }
 
 }
 
