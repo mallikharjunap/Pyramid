5fc5558072d4d615cf5b3676583fec0b0676957b
mallikharjunap <39264067+mallikharjunap@users.noreply.github.com>
2024-11-16 14:05:08 +0530

    left half pyramid

--- RightPyramid/RightPyramid/ViewController.swift
+++ RightPyramid/RightPyramid/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Right Pyramid
         for i in 1..<10 {
             for j in 1...i {
                // value = value + 1
@@ -20,6 +22,22 @@ class ViewController: UIViewController {
             print("")
             
         }
+        print("===========================================")
+        
+        // left pyramid
+        for i in 1..<10 {
+            
+            for j  in stride(from: 10, to: i, by: -1)
+            {
+                print(terminator: "  ")
+            }
+            for k in 1...i{
+                print("*", terminator:" ")
+            }
+            print("")
+        }
+        
+        print("===========================================")
     }
     
 
